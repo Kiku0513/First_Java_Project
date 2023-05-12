389df12b5657515879a12c270476debab569bccd
Komal <komaldhumale9333@gmail.com>
2023-05-12 12:41:36 +0530

    Varaibled n Datatypes

---
+++ src/learning_java/Variable_Datatypes.java
@@ -0,0 +1,25 @@
+package learning_java;
+
+public class Variable_Datatypes 
+{
+	public static void main(String[] args)
+	{
+		int travel_allowance=245678976;
+		char section='a';
+		float temperature=89.56f;
+		double heat=67.6789909;
+		short choclatePrice=89;
+		boolean b=true;
+		long carPrice=256788908;
+		System.out.println("Travel allowance are"+ travel_allowance);
+		System.out.println("My Kid Section is" +section);
+		System.out.println("Todays Temperature is " +temperature);
+		System.out.println("Heat to bake the cake" +heat);
+		System.out.println("Choclate price is" +choclatePrice);
+		System.out.println("Below sentece is true or false "+b);
+		System.out.println("Price of my car is"+carPrice);
+		
+		
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ashis
+ *
+ */
+module Java_Project {
+}
