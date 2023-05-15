34af0f9a4995247daf1a67b164b0a8ad5a43e473
Komal <komaldhumale9333@gmail.com>
2023-05-15 12:44:44 +0530

    Logical Operator

---
+++ src/learning_java/Logical_Operator.java
@@ -0,0 +1,29 @@
+package learning_java;
+
+public class Logical_Operator {
+
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=25;
+		int c=15;
+		boolean val=true;
+		System.out.println(" Not Operator fot val " + !val);
+		
+		if((a>b)&&(b>c))
+		{
+			System.out.println("if block executed");
+		}
+		else if ((a>b)||(b>c))
+		{
+			System.out.println(" Else if block Executed");
+		}
+		else
+			
+		{
+			System.out.println("Only Else bolck will Executed");
+		}
+
+	}
+
+}
