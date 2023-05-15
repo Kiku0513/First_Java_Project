2721934e98deb2cc7a00a2217743a13bcc8b81c1
Komal <komaldhumale9333@gmail.com>
2023-05-15 12:17:57 +0530

    Relational Operator

---
+++ src/learning_java/Relational_Operator.java
@@ -0,0 +1,17 @@
+package learning_java;
+
+public class Relational_Operator {
+
+	public static void main(String[] args) 
+	{
+		int num=20;
+		int num1=10;
+		System.out.println("num > num1 ---" + (num >num1));
+		System.out.println("num < num1 ---" + (num < num1));
+		System.out.println("num >= num1 ---" + (num >= num1));
+		System.out.println("num <= num1 ---" + (num <=num1));
+		System.out.println("num == num1 ---" + (num ==num1));
+		System.out.println("num != num1 ---" + (num !=num1));
+	}
+
+}
