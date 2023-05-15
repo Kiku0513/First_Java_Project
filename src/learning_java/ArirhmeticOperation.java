5ca3e54995fa98e277b09a47e389450ebfb2744a
Komal <komaldhumale9333@gmail.com>
2023-05-15 12:02:00 +0530

    Arithmatic op

---
+++ src/learning_java/ArirhmeticOperation.java
@@ -0,0 +1,31 @@
+package learning_java;
+
+public class ArirhmeticOperation {
+
+	public static void main(String[] args) 
+	{
+		int a=60;
+		int b=20;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a/b;
+		int g=a%b;
+	
+		System.out.println("Addition of two no is " + c);
+
+		System.out.println("Substraction of two no is " + d);
+
+		System.out.println("Multiplication of two no is " + e);
+
+		System.out.println("Division of two no is " + f);
+
+		System.out.println("Modulo of two no is " + g);
+
+		
+				
+	
+
+	}
+
+}
