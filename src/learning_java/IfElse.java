3a9f1cb2606be4051ab2746bc779ccc2f8fc48a2
Komal <komaldhumale9333@gmail.com>
2023-05-15 11:41:02 +0530

    If else n If Else If Program

---
+++ src/learning_java/IfElse.java
@@ -0,0 +1,19 @@
+package learning_java;
+
+public class IfElse {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		if(a>b) 
+		{
+			System.out.println("A is greater than B");
+		}
+		else
+		{
+			System.out.println("B is greater than A");
+		}
+	}
+
+}
---
+++ src/learning_java/IfElseIf.java
@@ -0,0 +1,23 @@
+package learning_java;
+
+public class IfElseIf {
+
+	public static void main(String[] args)
+    {
+		int a=20;
+		int b=30;
+		if (a>b)
+		{
+			System.out.println("A is greater than b");
+		}
+		else if(a<b)
+		{
+			System.out.println("B is greater than A");
+		}
+		else
+		{
+			System.out.println("A is eqaual to B");
+		}
+	}
+
+}
