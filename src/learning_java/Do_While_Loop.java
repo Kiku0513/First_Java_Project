079149e91cfceaafa57baf69aab4f92b94e7faed
Komal <komaldhumale9333@gmail.com>
2023-05-16 09:35:24 +0530

    While n Do While

---
+++ src/learning_java/Do_While_Loop.java
@@ -0,0 +1,16 @@
+package learning_java;
+
+public class Do_While_Loop {
+
+	public static void main(String[] args) 
+	{
+		int i=1;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+      while(i<=10);
+	}
+
+}
---
+++ src/learning_java/While_Loop.java
@@ -0,0 +1,15 @@
+package learning_java;
+
+public class While_Loop {
+
+	public static void main(String[] args)
+	{
+		int i=0;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
