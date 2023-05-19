5504dcb49cdb170f0eda3fcbd4709d09974e63c5
Komal <komaldhumale9333@gmail.com>
2023-05-19 17:12:08 +0530

    Assingment done by me

---
+++ src/assingment/Arihtmatic_Operation_Cons.java
@@ -0,0 +1,53 @@
+package assingment;
+
+public class Arihtmatic_Operation_Cons 
+{  
+	int a;
+	int b;
+	
+	Arihtmatic_Operation_Cons(int c,int d)
+	{
+		a=c;
+		b=d;
+	}
+	public int addition()
+	{
+		int add=a+b;
+		//System.out.println("Addition of two no is "+ add);
+		return add;
+	}
+	public void substraction()
+	{
+		int sub=a-b;
+		System.out.println("Substraction of two no is "+ sub);
+	}
+	public void multiplication()
+	{
+		int mul=a*b;
+		System.out.println("Multiplication  of two no is "+ mul);
+	}
+	public void division()
+	{
+		int div=(a/b);
+		System.out.println("Division of two no is "+ div);
+	}
+	public void moduelo()
+	{
+		int mod=(a%b);
+		System.out.println("Modulo of two no is "+ mod);
+	}
+	
+	public static void main(String[] args) 
+	{
+		 Arihtmatic_Operation_Cons obj1=new Arihtmatic_Operation_Cons(10,5);
+		 int plus=obj1.addition();
+		 System.out.println("Addition of two no is "+ plus);
+		 
+  obj1.substraction();
+  obj1.multiplication();
+  obj1.division();
+  obj1.moduelo();
+		 
+	}
+
+}
---
+++ src/assingment/Marks_Grade.java
@@ -0,0 +1,107 @@
+package assingment;
+
+public class Marks_Grade 
+{
+	
+	
+	//OWN SOLVE PROGRAM
+	/*
+	 * checkPassOrFail (student name, staudent mark)
+	 * {
+	 *  if(studentmark >= 40)
+	 *  {
+	 * 		pass
+	 * }
+	 * else
+	 * {
+	 * 	fail
+	 * }
+	 * 
+	 *  
+	 *  checkGrade
+	 *  
+	 *  if ( student is pass)
+	 *  
+	 *  {
+	 *  if (mark >==85)
+	 *  {
+	 *  grade a
+	 *  }
+	 *  else if(mark   >= 70 and mark <85)
+	 *  {
+	 *  grade b
+	 *  }
+	 *  else if (mark >=50 and mark <70)
+	 *  {
+	 *  grade c
+	 *  }
+	 *  else if (mark < 50)
+	 *  {
+	 *  grade d
+	 *  }
+	 *  }
+	 *  else
+	 *  {
+	 *  syso("Stuent has failed and hence grade is not given")
+	 *  }
+	 *
+	 */
+
+	String cadname;
+	int cadmarks;
+	public int checkPassorFail(String name,int marks)
+	{
+		cadname=name;
+		cadmarks=marks;
+
+		if(cadmarks>=40)
+		{
+			System.out.println("The name of the Student is " + cadname +" got marks "+ cadmarks +" is PASS in the Exam ");
+		}
+		else
+		{
+			System.out.println("Thn Student is FAAIL");
+		}
+		return cadmarks;
+	}
+
+	public int grade( int result) 
+	{ 
+		if(result==cadmarks)
+		{
+			if(cadmarks>=85)
+			{
+				System.out.println("Got Grade A");
+			}
+			else if(cadmarks>=70 && cadmarks<85)
+			{
+				System.out.println("Got Grade B");
+			}
+			else if(cadmarks>=50 &&cadmarks<70)
+			{
+				System.out.println(" Got Grade C");
+			}
+			else if(cadmarks>=40 && cadmarks<50)
+			{
+				System.out.println("Got Grade D");
+			}
+
+			else
+			{
+				System.out.println("Student is fail thats why there is no grade");
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) 
+	{
+		Marks_Grade obj1=new Marks_Grade();
+		int result=obj1.checkPassorFail("Komal",45);
+		obj1.grade(result);
+
+
+
+	}
+
+}
